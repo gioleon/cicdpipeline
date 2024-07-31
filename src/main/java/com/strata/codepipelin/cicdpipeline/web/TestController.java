22812377a11378b2bf3a949f864e5c029657031a
giovannileonp <giovanni.posada@strata-analytics.us>
2024-07-31 12:50:17 -0500

    first commit

---
+++ src/main/java/com/strata/codepipelin/cicdpipeline/web/TestController.java
@@ -0,0 +1,19 @@
+package com.strata.codepipelin.cicdpipeline.web;
+
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(path="/test")
+public class TestController {
+    
+    @GetMapping
+    public ResponseEntity<Map<String, String>> test() {
+        return ResponseEntity.ok().body(Map.of("testing", "up and running"));
+    }
+}
